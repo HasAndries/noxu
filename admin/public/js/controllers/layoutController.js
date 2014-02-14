9e725c86efd52594cd8bc83f2a7cca673bd483a0
Andries Coetzee <andriesc@lime-square.net>
2014-02-14 08:20:48 +0200

    starting new layout

--- admin/public/js/controllers/layoutController.js
+++ admin/public/js/controllers/layoutController.js
@@ -1,6 +1,10 @@
-LayoutController.$inject = ['$scope', '$rootScope'];
-function LayoutController($scope, $rootScope){
+LayoutController.$inject = ['$scope', '$rootScope', '$location'];
+function LayoutController($scope, $rootScope, $location){
   $rootScope.$on('display.mode', function(event, val){
     $scope.displayMode = val;
   });
+  console.log($location.absUrl());
+  console.log($location.hash());
+  console.log($location.path());
+  console.log($location.url());
 }
