0f9fcd5560af156885757bafbd77d5ce4653f5dd
Andries Coetzee <andriesc@lime-square.net>
2014-03-06 07:41:18 +0200

    threading implemented

--- network/network.js
+++ network/network.js
@@ -2,19 +2,20 @@ var extend = require('node.extend');
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 var fs = require('fs');
+var Fiber = require('fibers');
 var RF24 = require('../rf24');
 var Message = require('./message');
 var instructions = require('./instructions');
 
-function Network(config){
+function Network(config) {
   this.nodes = [];
   this.config = {
     bufferSize: 32,
     channel: 0x4c,
     dataRate: RF24.DataRate.kb1000,
     crcBytes: RF24.Crc.bit16,
-    retryCount: 0,
-    retryDelay: 0,
+    retryCount: 10,
+    retryDelay: 100,
     spiDev: '/dev/spidev0.0',
     pinCe: 25,
     address: 0x00F0F0F0F0
@@ -33,7 +34,7 @@ function Network(config){
   radio.setRetries(this.config.retryCount, this.config.retryDelay);
   radio.setPayloadSize(this.config.bufferSize);
   radio.enableDynamicPayloads();
-  radio.setAutoAck(false);
+  radio.setAutoAck(true);
   radio.powerUp();
   radio.openReadingPipe(1, this.config.address);
   radio.openWritingPipe(this.config.address);
@@ -44,24 +45,27 @@ function Network(config){
 util.inherits(Network, EventEmitter);
 
 //========== PUBLIC ==========
-Network.prototype.start = function(){
+Network.prototype.start = function () {
   this._start();
   this.running = true;
-  this._loop();
+  var _this = this;
+  Fiber(function(){
+    _this._loop(_this);
+  }).run();
 };
 
-Network.prototype.stop = function(){
+Network.prototype.stop = function () {
   this._stop();
   this.running = false;
 };
 
-Network.prototype.send = function(obj){
+Network.prototype.send = function (obj) {
   var list = obj instanceof Array && extend([], obj) || [obj];
   if (!list.length) return;
   //stop inbound
   if (this.running) this._stop();
   //write messages
-  for(var ct in list){
+  for (var ct in list) {
     var buffer = list[ct].toBuffer();
     this.radio.write(buffer);
     this.emit('outbound', buffer);
@@ -71,42 +75,48 @@ Network.prototype.send = function(obj){
 };
 
 //========== PRIVATE ==========
-Network.prototype._loop = function(){
-  //process inbound
-  if (this.listening){
-    var avail = this.radio.available();
-    if (avail.any){
-      var data = this.radio.read();
-      var buffer = new Buffer(this.config.bufferSize);
-      buffer.fill(0);
-      data.copy(buffer);
-      var message = new Message({data: data, bufferSize: this.config.bufferSize});
-      this.emit('inbound', buffer);
-      this._processInbound(message);
+Network.prototype._loop = function (_this) {
+  console.log('loop enter');
+  while (_this.running) {
+    //process inbound
+    if (_this.listening) {
+      var avail = _this.radio.available();
+      if (avail.any) {
+        //var start = process.hrtime();
+        var data = _this.radio.read();
+        //var diff = process.hrtime(start);
+        //console.log('read: %dms', (diff[0] * 1e9 + diff[1])/1000000);
+
+        var buffer = new Buffer(_this.config.bufferSize);
+        buffer.fill(0);
+        data.copy(buffer);
+        var message = new Message({data: data, bufferSize: _this.config.bufferSize});
+        _this.emit('inbound', buffer);
+        _this._processInbound(message);
+      }
+      //var message = new Message({fromCommander:true, instruction: 4, data: [0,1], bufferSize: 32});
+      //this.send([message]);
     }
-    //var message = new Message({fromCommander:true, instruction: 4, data: [0,1], bufferSize: 32});
-    //this.send([message]);
+    sleep(10);
   }
-  //loop
-  if (this.running)
-    setImmediate(this._loop.bind(this));
+  console.log('loop exit: ' + JSON.stringify(_this));
 };
 
-Network.prototype._start = function(){
+Network.prototype._start = function () {
   if (this.listening) return;
   this.radio.startListening();
   this.listening = true;
 };
 
-Network.prototype._stop = function(){
+Network.prototype._stop = function () {
   if (!this.listening) return;
-  this.radio.startListening();
-  this.listening = true;
+  this.radio.stopListening();
+  this.listening = false;
 };
 
-Network.prototype._processInbound = function(message){
+Network.prototype._processInbound = function (message) {
   console.log(message);
-  if(message.instruction == instructions.NETWORKID_REQ){
+  if (message.instruction == instructions.NETWORKID_REQ) {
     var tempId = message.data.readUInt32LE(0);
     if (this._getNodeTempId(tempId)) return;//know this tempId
     message.fromCommander = true;
@@ -117,14 +127,14 @@ Network.prototype._processInbound = function(message){
     this.emit('nodeNew', node);
     this.send(message);
   }
-  else if(message.instruction == instructions.NETWORKID_CONFIRM){
-    var node = this._getNode(message.hops[message.hops.length-1]);
+  else if (message.instruction == instructions.NETWORKID_CONFIRM) {
+    var node = this._getNode(message.hops[message.hops.length - 1]);
     node.confirmed = true;
     this._saveNodes();
     this.emit('nodeConfirm', node);
   }
-  else if(message.instruction == instructions.PING_REPLY){
-    var node = this._getNode(message.hops[message.hops.length-1]);
+  else if (message.instruction == instructions.PING_REPLY) {
+    var node = this._getNode(message.hops[message.hops.length - 1]);
     var diff = process.hrtime(node.pingStart);
     node.ping = diff[0] * 1e9 + diff[1];
     delete node.pingStart;
@@ -132,50 +142,58 @@ Network.prototype._processInbound = function(message){
   }
 };
 
-Network.prototype._newNode = function(tempId){
-  var newNode = {id: 1+this.nodes.length, tempId: tempId};
+Network.prototype._newNode = function (tempId) {
+  var newNode = {id: 1 + this.nodes.length, tempId: tempId};
   this.nodes.push(newNode);
   this._saveNodes();
   return newNode;
 };
-Network.prototype._getNode = function(id){
-  for(var ct=0;ct<this.nodes.length;ct++){
+Network.prototype._getNode = function (id) {
+  for (var ct = 0; ct < this.nodes.length; ct++) {
     if (this.nodes[ct].id == id) return this.nodes[ct];
   }
   return null;
 };
-Network.prototype._getNodeTempId = function(tempId){
-  for(var ct=0;ct<this.nodes.length;ct++){
+Network.prototype._getNodeTempId = function (tempId) {
+  for (var ct = 0; ct < this.nodes.length; ct++) {
     if (this.nodes[ct].tempId == tempId) return this.nodes[ct];
   }
   return null;
 };
-Network.prototype._deleteNode = function(id){
-  for(var ct=0;ct<this.nodes.length;ct++){
+Network.prototype._deleteNode = function (id) {
+  for (var ct = 0; ct < this.nodes.length; ct++) {
     if (this.nodes[ct].id == id) this.nodes.splice(ct, 1);
   }
   this._saveNodes();
 };
-Network.prototype._deleteNodesAll = function(){
+Network.prototype._deleteNodesAll = function () {
   this.nodes = [];
   this._saveNodes();
 };
-Network.prototype._saveNodes = function(){
+Network.prototype._saveNodes = function () {
   var json = JSON.stringify(this.nodes);
-  fs.writeFile('nodes.txt', json, function(err){
+  fs.writeFile('nodes.txt', json, function (err) {
     if (!err) this.emit('savedNodes');
     else this.emit('saveNodesError', err);
   }.bind(this));
 };
-Network.prototype._loadNodes = function(){
+Network.prototype._loadNodes = function () {
   if (!fs.existsSync('nodes.txt')) return;
-  fs.readFile('nodes.txt', function(err, data){
-    if (!err){
+  fs.readFile('nodes.txt', function (err, data) {
+    if (!err) {
       this.nodes = JSON.parse(data);
       this.emit('loadedNodes');
     }
     else this.emit('loadNodesError', err);
   }.bind(this));
 };
+
+function sleep(ms) {
+  var fiber = Fiber.current;
+  setTimeout(function () {
+    fiber.run();
+  }, ms);
+  Fiber.yield();
+}
 
 module.exports = Network;
