a351b9c9a031a4f316a69bd6d05f9ea9fc0a00d4
Andries Coetzee <andriesc@lime-square.net>
2014-05-20 22:44:33 +0200

    added coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,18 +24,34 @@ module.exports = function (grunt) {
         options: { stdout: true },
         command: 'npm install spi rpi-gpio'
       }
+    },
+    jasmine_node: {
+      coverage: {
+
+      },
+      options: {
+        forceExit: true,
+        match: '.',
+        matchall: false,
+        extensions: 'js',
+        specNameMatcher: 'spec',
+        captureExceptions: true
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+  grunt.loadNpmTasks('grunt-shell');
 
   grunt.registerTask('spi', ['shell:spi']);
 
   grunt.registerTask('build', ['clean:build','copy:admin', 'copy:network']);
   grunt.registerTask('build-admin', ['clean:build','copy:admin']);
   grunt.registerTask('build-network', ['clean:build', 'copy:network']);
+
+  grunt.registerTask('cov', ['jasmine_node']);
 
   grunt.registerTask('default', ['build']);
 };
