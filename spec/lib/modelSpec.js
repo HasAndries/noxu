c2d147ed439ad9354d73358625f80f8c3a0be4de
Andries Coetzee <andriesc@lime-square.net>
2014-06-02 13:39:11 +0200

    started with model

--- spec/lib/modelSpec.js
+++ spec/lib/modelSpec.js
@@ -1,8 +1,19 @@
+var util = require('util');
 var Model = require('../../lib/model');
 
 describe('Model', function(){
-  describe('loadAll', function(){
+  var options;
+  beforeEach(function(){
+    options = {
+      name: 'TestModel',
+      fields: ['testId', 'name', 'cell'],
+      id: 'testId',
+      limit:0,
 
+    }
+  });
+  describe('loadAll', function(){
+    var model = new Model({name: 'Test'});
   });
   describe('save', function(){
 
